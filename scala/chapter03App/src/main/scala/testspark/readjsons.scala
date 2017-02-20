167e0feced86f6c2340bc8f7ef74795a9f6e94b6
nikolausn <nikolaus.nova@gmail.com>
2017-02-19 22:30:44 -0600

    finishing tasks

--- scala/chapter03App/src/main/scala/testspark/readjsons.scala
+++ scala/chapter03App/src/main/scala/testspark/readjsons.scala
@@ -2,22 +2,30 @@ package testspark
 
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
+import scala.collection.mutable
+import org.apache.spark.sql.functions.explode
+import scala.collection.mutable.WrappedArray
 
 object readjsons extends App {
-  val sc = new SparkContext(master="spark://ontangs-i7.local:7077",appName="practice");
-  //val spark = SparkSession.builder().master("local").appName("practice").getOrCreate(); 
-  val spark = SparkSession.builder().master("spark://ontangs-i7.local:7077").appName("practice").getOrCreate(); 
+  // spark local
+  val sc = new SparkContext(master = "local", appName = "practice");
+  val spark = SparkSession.builder().master("local").appName("practice").getOrCreate();
+  val jsonFiles = sc.wholeTextFiles("/Volumes/HD-500GB/Users/nikolausn/Documents/msimsvn/msimrepo/lab-adm/python-converter/data/*/*/*/*.json", 3);
+
+  //val sc = new SparkContext(master="spark://sp17-cs511-02.cs.illinois.edu:7077",appName="practice");
+  //val spark = SparkSession.builder().master("spark://sp17-cs511-02.cs.illinois.edu:7077").appName("practice").getOrCreate(); 
   // Read json files itno rdd
-  val jsonFiles = sc.wholeTextFiles("/Volumes/HD-500GB/Users/nikolausn/Documents/msimsvn/msimrepo/lab-adm/data/*/*/*/*.json", 3);
-  
+  //val jsonFiles = sc.wholeTextFiles("hdfs://sp17-cs511-02.cs.illinois.edu:54310/musicds/*/*/*/*.json", 3);
+
   // convert jsonrdd into dataframe
   val jsonDF = spark.read.json(jsonFiles.values);
   //jsonDF.printSchema();
   jsonDF.createOrReplaceTempView("musicdb");
-  
+
   /*
-   * Schema
-   *  |-- get_analysis_sample_rate: long (nullable = true)
+   * Schema for the million song dataset after reading the json files
+   * 
+ |-- get_analysis_sample_rate: long (nullable = true)
  |-- get_artist_7digitalid: long (nullable = true)
  |-- get_artist_familiarity: double (nullable = true)
  |-- get_artist_hotttnesss: double (nullable = true)
@@ -65,11 +73,44 @@ object readjsons extends App {
  |-- get_track_id: string (nullable = true)
  |-- get_year: long (nullable = true)
    */
+
+  val artistSong = spark.sql("SELECT get_year,get_artist_name,count(get_artist_name) as total_music from musicdb GROUP BY get_year,get_artist_name order by get_year,total_music desc");
+  artistSong.show();
+  //artistSong.rdd.saveAsTextFile("artist_song.txt")
+  val artistHotness = spark.sql("SELECT get_artist_name,avg(get_artist_hotttnesss) as avg_hottness,avg(get_duration) as avg_duration from musicdb GROUP BY get_artist_name order by avg_hottness desc");
+  artistHotness.show()
+
   
-  val artistSong = spark.sql("SELECT get_artist_name,count(get_artist_name) from musicdb GROUP BY get_artist_name");
-  artistSong.show();  
-  val yearSong = spark.sql("SELECT get_year,count(get_year) from musicdb GROUP BY get_year");
-  yearSong.show();  
-  
-  
+  // third task, counting music terms
+  // we can use Spark SQL for this, need to use RDD and map reduce
+  // this works like wordcount but the complexity lies in the list of string for every RDD row
+  // val termsFlat = artistTerms.rdd.groupBy { x => x }.map( t => (t._1,t._2.size))
+  val artistTerms = spark.sql("SELECT get_artist_terms from musicdb");
+  artistTerms.show(); 
+  val termsFlat = artistTerms.rdd
+  val first = termsFlat.first()
+  val mapped = first.getAs[WrappedArray[Int]](0)
+  val test = mapped.mkString("\n")
+
+  // val termsString = termsFlat.map { x => x.getAs[WrappedArray[Int]](0).mkString("\n") }
+  // combine the terms
+  // after explode the terms for each artist and sign a weight 1 for each terms, we flatten the map function
+  // so it will combine all the values in the RDD
+  val termsString = termsFlat.map {
+    x =>
+      x.getAs[WrappedArray[String]](0).groupBy {
+        l => l
+      }.map(f => (f._1, f._2.length))
+  }.flatMap(identity)
+
+  //count the combined terms using reduceByKey, add the count value
+  val termsCount = termsString.reduceByKey { case (x, y) => x + y }
+
+  //print the result
+  println(termsCount.collect().mkString(","))
+
+  //print(termsFlat.first());
+  //val termsGroups = artistTerms.rdd.map { x => x.s } groupByKey
+  //musicType.rdd
+
 }
